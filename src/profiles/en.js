69acf2063f5257fa8aa0fb185092694a389d7485
Xingchen Hong <hello@xc-h.com>
2018-02-15 07:21:38 -0600

    Add dummy profile data.

---
+++ src/profiles/en.js
@@ -0,0 +1,10 @@
+module.exports = {
+    firstName: 'Xingchen',
+    lastName: 'Hong',
+    site: {
+        text: 'xc-h.com',
+        url: 'https://xc-h.com',
+    },
+    email: 'hello@xc-h.com',
+    phone: '+1 217-419-0746',
+};
---
+++ src/theme.js
@@ -0,0 +1,12 @@
+export default {
+  primaryColor: '#5EB886',
+  secondaryInfoColor: 'rgba(40, 40, 40, 0.7)',
+  serifFontFamily: [
+    'Roboto Slab',
+    'serif',
+  ],
+  sansSerifFontFamily: [
+    'Roboto',
+    'sans-serif',
+  ],
+};
