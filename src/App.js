8c834b7c0cb695fa7e8adfcc557d720abba66a4b
Xingchen Hong <hello@xc-h.com>
2018-02-15 12:38:09 -0600

    Add experience rendering (styling not done yet, data not fully loaded yet).

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import * as url from 'url';
 import React, { Component } from 'react';
 import classNames from '@xch/class-names';
 import injectSheet from 'react-jss';
+import moment from 'moment';
+import marked from 'marked';
 
 import logo from './logo.svg';
 import './App.css';
@@ -75,51 +77,162 @@ const AppStyles = (theme) => ({
       extend: '_subtleLink',
     },
   },
+  contentSectionTitle: {
+    color: theme.primaryColor,
+  },
+  experienceItem: {
+    '&.experience': {
+      display: 'grid',
+      gridTemplateAreas: [[
+        '"employer location title"',
+        '"timespan timespan timespan"',
+        '"duties   duties   duties"',
+      ]],
+      gridTemplateColumns: 'auto auto 1fr',
+
+      '& .experience__employer': {
+        gridArea: 'employer',
+      },
+      '& .experience__location': {
+        gridArea: 'location',
+      },
+      '& .experience__employer + .experience__location::before': {
+        content: '", "',
+      },
+      '& .experience__title': {
+        gridArea: 'title',
+      },
+      '& .experience__location + .experience__title::before': {
+        content: '""',
+        display: 'inline-block',
+        width: '0.5em',
+        height: '0',
+        marginLeft: '0.5em',
+        marginRight: '0.5em',
+        marginBottom: '0.3em',
+        borderTop: '0.1em solid',
+      },
+      '& .experience__timespan': {
+        gridArea: 'timespan',
+      },
+      '& .experience__duties': {
+        gridArea: 'duties',
+        fontFamily: theme.serifFontFamily,
+      },
+    },
+  },
 });
 
 class App extends Component {
-  render() {
-    const appStyleClasses = this.props.classes;
 
+  get styleClasses () {
+    return this.props.classes;
+  }
+
+  documentHeader = (pageProps) => {
+    return (
+      <div className={this.styleClasses.headerBar}></div>
+    );
+  };
+
+  documentFooter = (pageProps) => {
+    return (
+      <div className={this.styleClasses.footerRow}>
+        <label id="version"><a href={versionUrl} target="_blank">{npmManifest.name}@{npmManifest.version}</a></label>
+        <label
+          id="pagenumber"
+          className="pagenumber"
+        >
+          <span className="pagenumber__index">{pageProps.pageNumber}</span>
+          <span>&nbsp;/&nbsp;</span>
+          <span className="pagenumber__count">{pageProps.pageCount}</span>
+        </label>
+      </div>
+    );
+  };
+
+  renderExperienceItem = (expItem, index) => {
+    const {
+      title,
+      employer,
+      location,
+      startDate,
+      endDate,
+      duties,
+    } = expItem;
+
+    const [
+      startDateString,
+      endDateString,
+    ] = [
+      startDate,
+      endDate,
+    ].map((s) => s ? moment(s).format('MMM YYYY') : '');
+
+    return (
+      <div
+        key={index}
+        className={classNames(
+          'experience',
+          this.styleClasses.experienceItem,
+        )}
+      >
+        <span className="experience__employer">{employer}</span>
+        <span className="experience__location">{location}</span>
+        <span className="experience__title">{title}</span>
+        <div className="experience__timespan">
+          <span className="experience__timespan__start">{startDateString}</span>
+          <span>&nbsp;-&nbsp;</span>
+          <span className="experience__timespan__end">{endDateString || 'Present'}</span>
+        </div>
+        <div
+          className="experience__duties"
+          dangerouslySetInnerHTML={{__html: marked(duties)}}
+        />
+      </div>
+    );
+  };
+
+  render() {
     return (
       <Document
         className={classNames(
           'App',
-          appStyleClasses.document,
+          this.styleClasses.document,
         )}
-        pageClassName={appStyleClasses.page}
-        header={(props) => {
-          return (
-            <div className={appStyleClasses.headerBar}></div>
-          );
-        }}
-        footer={(props) => {
-          return (
-            <div className={appStyleClasses.footerRow}>
-              <label id="version"><a href={versionUrl} target="_blank">{npmManifest.name}@{npmManifest.version}</a></label>
-              <label
-                id="pagenumber"
-                className="pagenumber"
-              >
-                <span className="pagenumber__index">{props.pageNumber}</span>
-                <span>&nbsp;/&nbsp;</span>
-                <span className="pagenumber__count">{props.pageCount}</span>
-              </label>
-            </div>
-          );
-        }}
+        pageClassName={this.styleClasses.page}
+        header={this.documentHeader}
+        footer={this.documentFooter}
       >
         <Page>
-          <h1 className="profile__name">
-            <span className="profile__firstname">{profile.firstName}&nbsp;</span>
-            {profile.middleName && (
-              <span className="profile__middlename">{profile.middleName}&nbsp;</span>
-            )}
-            <span className="profile__lastname">{profile.lastName}</span>
-          </h1>
-          <div className="profile__site"><a href={profile.site.url} target="_blank">{profile.site.text}</a></div>
-          <div className="profile__phone">{profile.phone}</div>
-          <div className="profile__email"><a href={`mailto:${profile.email}`}>{profile.email}</a></div>
+          <section id="basic-info">
+            <h1 id="profile-name" className="profile__name">
+              <span className="profile__firstname">{profile.firstName}&nbsp;</span>
+              {profile.middleName && (
+                <span className="profile__middlename">{profile.middleName}&nbsp;</span>
+              )}
+              <span className="profile__lastname">{profile.lastName}</span>
+            </h1>
+            <div className="profile__site"><a href={profile.site.url} target="_blank">{profile.site.text}</a></div>
+            <div className="profile__phone">{profile.phone}</div>
+            <div className="profile__email"><a href={`mailto:${profile.email}`}>{profile.email}</a></div>
+          </section>
+
+          {profile.experiences && (
+            <section
+              id="experiences"
+              className="content-section"
+            >
+              <h2
+                className={classNames(
+                  'content-section__title',
+                  this.styleClasses.contentSectionTitle,
+                )}
+              >Experience</h2>
+
+              {profile.experiences.map(this.renderExperienceItem)}
+            </section>
+          )}
         </Page>
         <Page>
           <div className="App">
--- src/profiles/en.js
+++ src/profiles/en.js
@@ -7,4 +7,40 @@ module.exports = {
     },
     email: 'hello@xc-h.com',
     phone: '+1 217-419-0746',
+    experiences: [
+      {
+        title: 'Research Developer',
+        employer: 'National Center for Supercomputing Applications',
+        location: 'Urbana, Illinois, United States',
+        startDate: '2015-06',
+        endDate: false,
+        // Markdown.
+        duties:
+`
+- Proposed and laid the foundation of a new web development toolkit project called Web GIS Components. The project uses advanced web technologies such as Custom Elements and Shadow DOM to provide a suite of reusable components for developing web GIS applications.
+- Developed a new prototype web application for the SKOPE project using Meteor and React for more efficient and flexible development process. The web application focuses on visualizing data variations over time.
+- Developed data extractor software for a big data management platform of the TerraRef project, extending its the data processing capabilities.
+- Took charge in training colleagues about developing Web applications. Prepared a few coding challenges involving using Meteor to build a simple single-page web app with live-updating features and another headless ticketing app with REST APIs. The training uses Git, Webhooks and Continuous Integration to automatically grade submissions. They can be found here: https://goo.gl/fFa5Yz
+- Prepared guidelines on web animation design and implementation principles.
+- Improved web application deployment and testing process by incorporating Docker containerization.
+- Designed and developed a web application called Map Visualizer that can visualize map layers of various data types based on simple config files. The project can be found here: https://goo.gl/TL4vn9
+- Worked with researchers on a web GIS app that analyzes and visualizes radiation anomalies from live-streamed geospatial sensor data.
+- Led a team developing TopoLens, a web GIS app, using MeteorJS, Openlayers and Material Design Lite, that serves on-demand, reprojected and resampled geographical elevation data. Proposed the high-level architecture of the service that is decentralized and uses message queues and the concept of workers to improve throughput, availability and scalability.
+`,
+      },
+      {
+        title: 'Student Developer',
+        employer: 'National Center for Supercomputing Applications',
+        location: 'Urbana, Illinois, United States',
+        startDate: '2014-03',
+        endDate: '2015-05',
+        // Markdown
+        duties:
+`
+- Designed and implemented a prototype “visual workflow constructor”, using ExtJS and SigmaJS, for DEM infrastructure running on HPC clusters. The workflow constructor takes in available outputs and builds the final workflow based on a complex dependency graph.
+- Designed and implemented a new wizard UI for submitting a job request in the TauDEM app. The wizard breaks the entire dynamic form into multiple small stages that are easier to complete and also allows the user to go back to any previous stages.
+- Designed and implemented a new UI for the CyberGIS Gateway, an online platform for web GIS applications, using ExtJS.
+`,
+      },
+    ],
 };
