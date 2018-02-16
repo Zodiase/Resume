ad7a12c70d4b410f4742ea7af4cb4f043ec491c3
Xingchen Hong <hello@xc-h.com>
2018-02-16 00:47:30 -0600

    Restrict extra styles to screen only.

--- src/Document.js
+++ src/Document.js
@@ -11,17 +11,19 @@ import {
 
 const DocumentStyles = {
   root: {
-    '&.document': {
-      padding: '1px',
-      backgroundColor: 'rgba(0, 0, 0, 0.1)',
+    '@media screen': {
+      '&.document': {
+        padding: '1px',
+        backgroundColor: 'rgba(0, 0, 0, 0.1)',
 
-      '& .document__page': {
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        marginTop: '2em',
-        marginBottom: '2em',
-        boxShadow: `rgba(0, 0, 0, 0.19) 0px ${10/16}em ${30/16}em, rgba(0, 0, 0, 0.23) 0px ${6/16}em ${10/16}em`,
-        borderRadius: `${2/16}em`,
+        '& .document__page': {
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          marginTop: '2em',
+          marginBottom: '2em',
+          boxShadow: `rgba(0, 0, 0, 0.19) 0px ${10/16}em ${30/16}em, rgba(0, 0, 0, 0.23) 0px ${6/16}em ${10/16}em`,
+          borderRadius: `${2/16}em`,
+        },
       },
     },
   },
